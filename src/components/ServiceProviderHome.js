f98ef4fee7bf1c144d65227f182701b48dc705a8
Shai <44603907+shaiidan@users.noreply.github.com>
2021-07-19 12:06:59 +0300

    Update ServiceProviderHome.js

--- src/components/ServiceProviderHome.js
+++ src/components/ServiceProviderHome.js
@@ -20,8 +20,8 @@ class ServiceProviderHome extends Component{
   }
   async componentDidMount() {
     // loading blockchain
-    const blockchian = await loadWeb3();
-    if(blockchian !== null) {
+    const blockchain = await loadWeb3();
+    if(blockchain !== null) {
       const blockchainData = await loadBlockchainData();
       if(blockchainData !== undefined ||blockchainData !== null){
         this.permissions = blockchainData.permissions; // save smart contruct
