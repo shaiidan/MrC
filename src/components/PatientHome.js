c04bbbc0b98b0a24a360dadbb42328603dd43b75
Shai <44603907+shaiidan@users.noreply.github.com>
2021-07-19 11:21:08 +0300

    Update PatientHome.js

--- src/components/PatientHome.js
+++ src/components/PatientHome.js
@@ -15,9 +15,9 @@ class PatientHome extends Component{
   
   async componentDidMount() {
     // loading blockchain 
-    await loadWeb3();
-    const blockchain = await loadBlockchainData();
-    if(blockchain !== undefined ||blockchain !== null){
+    const blockchian = await loadWeb3()
+    const blockchainData = await loadBlockchainData()
+    if(blockchainData !== undefined ||blockchainData !== null || blockchian !== null){
       this.permissions = blockchain.permissions; // save smart contruct
       let state = await loadState();
       const newState = $.extend(this.state,state);
